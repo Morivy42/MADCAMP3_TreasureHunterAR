f078ce33d5bef02e7193f82a58cf2ed6dba59ab7
Mo Jihun <jihunmo@kaist.ac.kr>
2023-07-15 14:11:55 +0900

    make 3 tab Home, AR, Collections Camera permission Camera preview image analyzer example mesh animation collection sample

--- app/src/main/java/io/madcamp/treasurehunterar/MainActivity.kt
+++ app/src/main/java/io/madcamp/treasurehunterar/MainActivity.kt
@@ -1,46 +1,110 @@
 package io.madcamp.treasurehunterar
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import io.madcamp.treasurehunterar.ui.theme.TreasureHunterARTheme
 
 class MainActivity : ComponentActivity() {
+    private val colorViewModel by viewModels<ColorViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TreasureHunterARTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+//        super.onCreate(savedInstanceState)
+//        if (allPermissionsGranted()) {
+//            setViewContent()
+//        } else {
+//            ActivityCompat.requestPermissions(
+//                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+//            )
+//        }
+        setViewContent()
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        window.decorView.postDelayed({
+            window.decorView.systemUiVisibility = FLAGS_FULLSCREEN
+        }, IMMERSIVE_FLAG_TIMEOUT)
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                setViewContent()
+            } else {
+                Toast.makeText(
+                    this,
+                    getString(R.string.permission_message),
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TreasureHunterARTheme {
-        Greeting("Android")
+    private fun setViewContent() {
+    setContent {
+        TreasureHunterARTheme {
+            // A surface container using the 'background' color from the theme
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                TreasureHunterARApp(colorViewModel)
+            }
+        }
     }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
+        private const val IMMERSIVE_FLAG_TIMEOUT = 500L
+        const val FLAGS_FULLSCREEN =
+            View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
+
 }
+
+
+// original onCreated
+//override fun onCreate(savedInstanceState: Bundle?) {
+//    super.onCreate(savedInstanceState)
+//    setContent {
+//        TreasureHunterARTheme {
+//            // A surface container using the 'background' color from the theme
+//            Surface(
+//                modifier = Modifier.fillMaxSize(),
+//                color = MaterialTheme.colorScheme.background
+//            ) {
+//                TreasureHunterARApp()
+//            }
+//        }
+//    }
+//}
